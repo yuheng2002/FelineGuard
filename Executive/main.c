9092a586377433a80a2775aebecf693dcd05f226
Yuheng Zhang <yuheng.zhang2002@gmail.com>
2026-08-02 10:24:43 -0700

    Move toolchain support files into System/

--- Executive/main.c
+++ Executive/main.c
@@ -19,10 +19,6 @@
 #include <stdint.h>
 #include "stm32f4xx_hal.h"
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
-
 void SysTick_Handler(void)
 {
 	HAL_IncTick();
