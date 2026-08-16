7dbb78da0da9f71798b1d9336579ea3b54bbb0f2
Yuheng Zhang <yuheng.zhang2002@gmail.com>
2026-08-15 17:25:50 -0700

    add arbitration FSM logic

--- Application/Inc/Feed.h
+++ Application/Inc/Feed.h
@@ -1,6 +1,15 @@
 #ifndef INC_FEED_H_
 #define INC_FEED_H_
 
+#include <stdbool.h>
 
+typedef enum{
+	FEED_CMD,
+	FEED_BUTTON,
+	FEED_RTC
+}Feed_Source;
+
+bool Feed_Request(Feed_Source src);
+void Feed_Poll(void);
 
 #endif /* INC_FEED_H_ */
---
+++ Application/Src/Feed.c
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#include "Feed.h"
+#include "MOTOR_CTRL.h"
+#include "TIMER.h"
+
+#define FEED_TIME_MS   5000
+
+typedef enum {
+	FEED_STATE_IDLE,
+	FEED_STATE_FEEDING,
+	FEED_STATE_PENDING
+}Feed_State;
+
+static Feed_State curr_state = FEED_STATE_IDLE;
+
+static void start_feed(void){
+	MOTOR_Start();
+	TIMER_StartTimeout(FEED_TIME_MS);
+}
+
+bool Feed_Request(Feed_Source src){
+	switch (curr_state){
+		case FEED_STATE_IDLE:
+			start_feed();
+			curr_state = FEED_STATE_FEEDING;
+			return true;
+
+		case FEED_STATE_FEEDING:
+			/* only RTC request can be deferred */
+			if (src == FEED_RTC){
+				curr_state = FEED_STATE_PENDING;
+				return true;
+			}else{
+				return false;
+			}
+
+		case FEED_STATE_PENDING:
+			/* only one pending alarm; any request is dropped */
+			return false;
+
+		default:
+			return false;
+	}
+}
+
+void Feed_Poll(void){
+	/* do nothing if idle */
+	if (curr_state == FEED_STATE_IDLE) return;
+
+	/* do nothing if last feed is still running */
+	if (TIMER_InProg())
+	{
+		return;
+	}
+	/* if neither of above, a feed just completed */
+	MOTOR_Stop();
+	if (curr_state == FEED_STATE_PENDING){
+		start_feed();
+		curr_state = FEED_STATE_FEEDING;
+	}
+	else if (curr_state == FEED_STATE_FEEDING){
+		curr_state = FEED_STATE_IDLE;
+	}
+}
