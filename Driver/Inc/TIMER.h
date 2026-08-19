3ffcd205af7f3f4735c2b1eb727813f3552722d3
Yuheng Zhang <yuheng.zhang2002@gmail.com>
2026-08-18 20:15:43 -0700

    add comment

--- Driver/Inc/TIMER.h
+++ Driver/Inc/TIMER.h
@@ -8,7 +8,7 @@
  * it counts time inside the chip and does not require output pins */
 
 void TIMER_Init(void);
-void TIMER_StartTimeout(uint32_t ms);
+void TIMER_StartTimeout(uint32_t ms); /* ms must be > 0 */
 bool TIMER_InProg(void);
 
 #endif /* INC_TIMER_H_ */
