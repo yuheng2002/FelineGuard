a1759e970b1b3ef764ddab0b616321ff270fb04c
Yuheng Zhang <yuheng.zhang2002@gmail.com>
2026-09-21 17:47:51 -0700

    changing FreeRTOS heap size from 4KB to 8KB

--- FreeRTOS/Inc/FreeRTOSConfig.h
+++ FreeRTOS/Inc/FreeRTOSConfig.h
@@ -287,7 +287,7 @@
  * or heap_4.c are included in the build.  This value is defaulted to 4096 bytes
  * but it must be tailored to each application.  Note the heap will appear in
  * the .bss section.  See https://www.freertos.org/a00111.html. */
-#define configTOTAL_HEAP_SIZE                        4096
+#define configTOTAL_HEAP_SIZE                        8192
 
 /* Set configAPPLICATION_ALLOCATED_HEAP to 1 to have the application allocate
  * the array used as the FreeRTOS heap.  Set to 0 to have the linker allocate
